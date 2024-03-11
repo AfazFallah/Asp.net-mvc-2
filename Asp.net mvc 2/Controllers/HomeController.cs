947c3c13804fd42e44fd14bd8767a69d40a8403c
Afazfallah <afallah488@gmail.com>
2024-03-11 18:34:43 +0330

    feet-config code

--- Asp.net mvc 2/Controllers/HomeController.cs
+++ Asp.net mvc 2/Controllers/HomeController.cs
@@ -11,12 +11,27 @@ namespace Asp.net_mvc_2.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<Students> studentList = new List<Students>() 
+            {
+                new Students(1,"afaz" , "fallah" , 19),
+                new Students(2 , "yasin" , "Abedini" , 18),
+                new Students(3 , "reza" , "nekoohi" , 20),
+                new Students(4 , "mehdi" , "kalantari" , 18)
+            };
+            return View(studentList);
         }
         public ActionResult Login()
         {
-            Students students = new Students(1, "afaz", "family", 19);
+            Students students = new Students(1, "afaz", "fallah", 19);
             return View(students);
+        }
+        public ActionResult DeleteUser()
+        {
+            return View();
+        }
+        public ActionResult UpgradeUser()
+        {
+            return View();
         }
 
     }
